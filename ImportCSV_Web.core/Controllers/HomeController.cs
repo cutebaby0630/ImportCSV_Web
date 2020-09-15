7e5ce4281d5f7ffa117fdaf6114170966d7d455a
Vicky Yin <v-vyin@microsoft.com>
2020-09-15 18:03:04 +0800

    file upload error

--- ImportCSV_Web.core/Controllers/HomeController.cs
+++ ImportCSV_Web.core/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,39 +12,51 @@ using System.IO;
 
 namespace ImportCSV_Web.core.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly Models.ImportService importService = new ImportService();
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly Models.ImportService importService = new ImportService();
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Index(IFormFile img)
-        {
-            var name = img.Name;
-            var fileFileName = img.FileName;
-            var fileLength = img.Length;
-            var fileContentType = img.ContentType;
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
-}
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index([FromForm] Dictionary<string, string> fields, IFormFileCollection files)
+    {
+      // IFormFile.Name: 此檔案欄位在表單中的名稱
+      // IFormFile.FileName: 檔案來源名稱 (無路徑)
+      // IFormFile.Length: 檔案大小
+      foreach (var file in files)
+      {
+        string filename = file.FileName;//取得檔名字
+        var tablename = filename.Replace(".csv", "");
+
+        if (file.Length > 0)
+        {
+
+          var stream = new FileStream(Path.GetFileName("TempFile"), FileMode.Open);
+            file.CopyToAsync(stream);
+          
+        }
+      }
+      return View();
+    }
+      public IActionResult Privacy()
+      {
+        return View();
+      }
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+    }
+  }
